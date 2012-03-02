4d070144c391ccae89ff1b79c3f54dfc0016b357
deafwing <deafwing@gmail.com>
2012-03-02 00:37:33 -0500

    Updated Button
    Application will close on button press and send close to WIFI Status …
    validation required on iPhone … would like to store status on icon ….

--- WiFi-Switch/WiFi-Switch/HDViewController.m
+++ WiFi-Switch/WiFi-Switch/HDViewController.m
@@ -12,8 +12,8 @@
 
 @end
 
-@implementation HDViewController
-
+@implementation HDViewController 
+    
 @synthesize label;
 
 -(IBAction)pushBTN:(id)sender{
@@ -21,7 +21,32 @@
         label.text = @"WIFI Switch Off";
     else
         label.text = @"WIFI Switch On";
+        //NEED TO STORE THE STATUS IN PLIST ... 
+        //WOUD LIKE TO DISPLAY ICON STATUS AS WELL ... 
+        
+        #if TARGET_IPHONE_SIMULATOR
+                //exit( EXIT_SUCCESS ) ;
+        #else
+                /* this works in iOS 4.2.3 */
+                Class BluetoothManager = objc_getClass( "BluetoothManager" ) ;
+                id btCont = [BluetoothManager sharedInstance] ;
+                [self performSelector:@selector(toggle:) withObject:btCont afterDelay:0.1f] ;
+        #endif
+                return YES ;
+
+            
+        #if TARGET_IPHONE_SIMULATOR
+        #else
+            - (void)toggle:(id)btCont
+            {
+                BOOL currentState = [btCont enabled] ;
+                [btCont setEnabled:!currentState] ;
+                [btCont setPowered:!currentState] ;
+                exit( EXIT_SUCCESS ) ;
+            }
+        #endif
 }
+
 
 
 
@@ -29,6 +54,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)viewDidUnload
